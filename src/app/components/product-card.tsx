8cad77727d6b1260da7712a5d7451088cd134772
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-12-08 22:00:10 +0530

    Product card datas added

--- src/app/components/product-card.tsx
+++ src/app/components/product-card.tsx
@@ -8,16 +8,12 @@ export default function Productcard() {
 
   return (
     <div className="p-8">
-      {/* Header */}
       <h1 className="text-xl font-bold mb-4">
         Home . <span className="text-gray-500">Art de la table</span>
       </h1>
 
-      {/* Main Content */}
-      <div className="flex flex-wrap gap-6">
-        {/* Image Section */}
+      <div className="flex flex-wrap gap-10">
         <div className="flex gap-4">
-          {/* Left: Small Images */}
           <div className="flex justify-evenly flex-col gap-4">
             {images.map((image, index) => (
               <div key={index} className="shadow-lg p-2">
@@ -30,73 +26,101 @@ export default function Productcard() {
               </div>
             ))}
           </div>
-          {/* Right: Large Image */}
-          <div className="shadow-lg p-4">
+          <div className="shadow-lg p-4 rounded-lg">
             <Image
               src="https://s3-alpha-sig.figma.com/img/a697/3509/f0a6397c22547c66edb2d8aa2737a300?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kAgvBKhptyNn2X9wgoUT-2AImt5tPN~DqXmg9ivpm7afXoMGoMccm87TaTIdmy3DVZ2kt62od2NsKxgv8d2xJWMexc2CldAnLMpI2uEHIYxZNe~DKesHflJm7g37IrCHmdXH3P-bVYeL-xK5tTUfbx2RO5Tw4NbRRtpJoMm-6ElwvG98gmIwH-4ptk9z-znzlHf9hyS9jqtKnJ6XdszWzlOVnz~YKnG8a2I1C6uZlt8m3~LHTzwx8Oj7mugjPlLqU8ZyHuGXsDWCKl7jrzeaj9Bl9sdr4ZnVbJgPI1zL~cQlce7eM0BSjgegYhzuKS8boN8eGR5vwL64WcQYlqHUNw__"
               width={500}
               height={500}
-              alt="Large Product Image"
+              alt=""
             />
           </div>
         </div>
 
-        {/* Description Section */}
         <div className="flex-1 space-y-4">
-          <div>
-            <h1 className="text-3xl font-bold">
-              Cheese - Appareil à raclette 1/2 roue
-            </h1>
-            <h1 className="text-2xl text-gray-800">
-              39,50{" "}
-              <span className="text-xl text-gray-500 font-light">/pièce</span>
-            </h1>
-          </div>
-          <div className="flex items-center gap-4">
-            <FaRuler className="text-gray-600" />
+          <div className="flex-col justify-end">
             <div>
+              <h1 className="text-3xl font-bold">
+                Cheese - appareil à raclette 1/2 roue
+              </h1>
+              <h1 className="font-semibold mt-3 text-2xl text-gray-800 border-b-2 pb-3">
+                39,50€
+                <span className="text-sm font-semibold text-gray-500 mb-4">
+                  {" "}
+                  /pièce
+                </span>
+              </h1>
+            </div>
+            <div className="flex items-center gap-4 text-lg">
+              <div className="flex gap-10 border-b-2 w-full pb-3">
+                <div className="flex gap-2">
+                  <FaRuler className="text-gray-600 text-2xl" />
+                  <div className="relative">
+                    <p className="text-base font-semibold">
+                      20
+                      <span className="absolute top-0 left-4 text-xs text-gray-500 transform translate-x-1 -translate-y-1">
+                        cm
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <svg
+                    width="25"
+                    height="25"
+                    viewBox="0 0 100 100"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="50"
+                      cy="50"
+                      r="40"
+                      stroke="black"
+                      strokeWidth="2"
+                      fill="none"
+                    />
+                    <line
+                      x1="85"
+                      y1="15"
+                      x2="15"
+                      y2="85"
+                      stroke="black"
+                      strokeWidth="2"
+                    />
+                  </svg>
+                  <div className="relative">
+                    <p className="text-base font-semibold">
+                      50
+                      <span className="absolute top-0 left-4 text-xs text-gray-500 transform translate-x-1 -translate-y-1">
+                        cm
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="text-gray-700 text-sm">
               <p>
-                20 <span className="text-gray-500">cm</span>
+                Location appareil à raclette - Raclette traditionnelle 1/2 roue
               </p>
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 100 100"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle
-                  cx="50"
-                  cy="50"
-                  r="40"
-                  stroke="black"
-                  strokeWidth="2"
-                  fill="none"
-                />
-                <line
-                  x1="85"
-                  y1="15"
-                  x2="15"
-                  y2="85"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-              </svg>
-              <p>
-                50 <span className="text-gray-500">cm</span>
-              </p>
+              <p>Réglable en hauteur</p>
+              <p>Appareil à raclette professionnel</p>
+              <p>Boîtier de chauffe horizontal réglable en hauteur</p>
+            </div>
+            <div className="mt-6 text-gray-700">
+              <p>200V</p>
+              <p>900W</p>
             </div>
           </div>
-          <div className="text-gray-700 space-y-2">
-            <p>
-              Location appareil à raclette - Raclette traditionnelle 1/2 roue
-            </p>
-            <p>Réglable en hauteur</p>
-            <p>Appareil à raclette professionnel</p>
-            <p>Boîtier de chauffe horizontal réglable en hauteur</p>
-          </div>
-          <div className="mt-6 text-gray-700">
-            <p>200V</p>
-            <p>900W</p>
+          <div className="flex justify-between gap-2">
+            <div className="flex items-center gap-10 outline outline-1 outline-gray-400 rounded-md p-1">
+              <button className="text-2xl p-1">-</button>
+              <h1 className="text-xl font-bold flex-shrink-0">1</h1>
+              <button className="text-2xl p-1">+</button>
+            </div>
+
+            <div className="bg-[#5CD2DD] w-full text-center rounded-md flex items-center justify-center">
+              <button className="px-4 py-2">ajouter au panier</button>
+            </div>
           </div>
         </div>
       </div>
