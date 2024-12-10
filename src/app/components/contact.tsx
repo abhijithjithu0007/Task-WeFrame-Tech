9c16aa339c2abd2a2afb625b2bc4ef4cbb89d348
abhijithjithu0007 <abhijithabhi36151@gmail.com>
2024-12-10 15:33:00 +0530

    Resolved a bug

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -1,23 +1,12 @@
 //contact page
 
-"use client";
-import { useEffect } from "react";
-import initializeAOS from "../utils/aosConfig"; //reusable animation
 import Image from "next/image";
 import { IoArrowForwardSharp } from "react-icons/io5";
 
 export default function Contact() {
-  //aos animation
-  useEffect(() => {
-    initializeAOS();
-  }, []);
-
   return (
-    <div
-      className="p-5 md:p-10 flex flex-col lg:flex-row gap-5 items-center justify-center"
-      data-aos="fade-up"
-    >
-      <div className="md:w-1/2" data-aos="fade-right">
+    <div className="p-5 md:p-10 flex flex-col lg:flex-row gap-5 items-center justify-center">
+      <div className="md:w-1/2">
         <div className="rounded-lg md:rounded-3xl overflow-hidden">
           <Image
             src="/assets/images/contactimg.jpeg"
@@ -29,10 +18,7 @@ export default function Contact() {
           />
         </div>
       </div>
-      <div
-        className="bg-[#FFF3F9] p-7 rounded-lg md:rounded-3xl md:w-1/2"
-        data-aos="fade-left"
-      >
+      <div className="bg-[#FFF3F9] p-7 rounded-lg md:rounded-3xl md:w-1/2">
         <h2 className="text-[25px] md:text-4xl text-gray-600">
           S'inscrire & économiser <span className="text-[#5CD2DD]">10%</span>
         </h2>
@@ -43,14 +29,14 @@ export default function Contact() {
           synergy highlights lunch. Company another pushback items dear or any.
         </p>
         <div className="mt-4 md:mt-8 flex flex-col md:flex-row items-center gap-3">
-          <div className="flex-grow" data-aos="fade-up">
+          <div className="flex-grow">
             <input
               type="text"
               placeholder="john@deo.com"
               className="w-full px-2 py-3 md:py-3 border border-gray-300 rounded-md"
             />
           </div>
-          <div className="w-full md:w-auto" data-aos="zoom-in">
+          <div className="w-full md:w-auto">
             <button
               className="uppercase w-full bg-[#5CD2DD] text-white py-3 md:py-3 px-5 
              flex items-center justify-center rounded-md
